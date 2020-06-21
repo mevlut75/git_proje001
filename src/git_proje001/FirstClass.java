94d690908e12b2d191bb0fa28c16d9f4ebec9fe3
mevlut <mevsui@hotmail.com>
2020-06-21 21:43:07 +0200

    suicmezde commit

--- src/git_proje001/FirstClass.java
+++ src/git_proje001/FirstClass.java
@@ -3,8 +3,12 @@ package git_proje001;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("ilk from ");
-
+		System.out.println("ilk from lokal repository");
+		System.out.println("ikinci from lokal reporisitory");
+		
+		System.out.println("yeni branch olusturdum");
+		System.out.println("git be");
+		System.out.println("yeni commit");
 	}
 
 }
